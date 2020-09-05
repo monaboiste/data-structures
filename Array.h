16cf4846955e29dcd06990445a8d2cdf552afcba
monaboiste <monikaszumna4@gmail.com>
2020-09-05 16:49:32 +0200

    Restored Array.h file

---
+++ Array.h
@@ -0,0 +1,111 @@
+#include <cstddef>
+#include <initializer_list>
+#include <cassert>
+
+#include "RAIterator.h"
+
+///
+/// Array class
+///
+/// Objects of this class have fixed non-modifiable size.
+/// Data is stored on the stack.
+///
+
+template <class T, size_t N>
+class Array
+{
+public:
+	using value_type = T;
+	using size_type = size_t;
+	using pointer = T*;
+	using reference = T&;
+	using const_pointer = const T*;
+	using const_reference = const T&;
+	using iterator = RAIterator<T>;
+	using const_iterator = RAIterator<const T>;
+
+public:
+	Array() = default;
+
+	Array(const std::initializer_list<value_type>& valueList)
+	{
+		assert(valueList.size() <= N);
+		std::copy(valueList.begin(), valueList.end(), m_data);
+	}
+
+	Array(const Array& other)
+	{
+		for (size_type i = 0; i < N; i++)
+			m_data[i] = other.m_data[i];
+	}
+
+	Array operator =(const Array& other)
+	{
+		for (size_type i = 0; i < N; i++)
+			m_data[i] = other.m_data[i];
+		return *this;
+	}
+
+	Array(const Array&& other)
+	{
+		for (size_type i = 0; i < N; i++)
+			m_data[i] = std::move(other.m_data[i]);
+	}
+
+	Array operator =(const Array&& other)
+	{
+		for (size_type i = 0; i < N; i++)
+			m_data[i] = std::move(other.m_data[i]);
+		return *this;
+	}
+
+	constexpr size_type Size() const
+	{
+		return N;
+	}
+
+	constexpr iterator Begin()
+	{
+		return iterator(&m_data[0]);
+	}
+
+	constexpr iterator End()
+	{
+		return iterator(&m_data[N]);
+	}
+
+	constexpr const_iterator Cbegin() const
+	{
+		return const_iterator(&m_data[0]);
+	}
+
+	constexpr const_iterator Cend() const
+	{
+		return const_iterator(&m_data[N]);
+	}
+
+
+	constexpr reference operator[] (size_type index)
+	{
+		assert(index >= 0 && index < N);
+		return m_data[index];
+	}
+
+	constexpr const_reference operator[] (size_type index) const
+	{
+		assert(index >= 0 && index < N);
+		return m_data[index];
+	}
+
+private:
+	value_type m_data[N];
+};
+
+// Array of size 0 is not allowed.
+
+template <class T>
+class Array<T, 0>
+{
+public:
+	Array() = delete;
+};
